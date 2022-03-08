b4d42e1fed1a906aa5048d1b5511a4575d36297e
Cesar Augusto Rayo Zapata <cesar-rayo@hotmail.es>
2022-03-08 17:21:51 -0500

    adding dummy server

--- pickle_api.py
+++ pickle_api.py
@@ -1,6 +1,6 @@
 import pickle
 import base64
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect, url_for, render_template
 
 app = Flask(__name__)
 
@@ -14,4 +14,31 @@ def index():
     return jsonify(data="unpickled")
 
 
-app.run(debug=True)
+@app.route('/success/<name>')
+def success(name):
+    return 'welcome %s' % name
+
+
+@app.route('/unauthorized')
+def unauthorized():
+    return 'NO ACCESS 🔐'
+
+
+@app.route("/authenticate", methods=["POST", 'GET'])
+def authenticate():
+    if request.method == "POST":
+        name = request.form['name']
+        try:
+
+            assert request.form["name"] == "carz"
+            print(url_for('success', name=name))
+            return redirect(url_for('success', name=name))
+        except AssertionError:
+            print("⚠️  NO ACCESS  ⚠️")
+            return redirect(url_for('unauthorized'))
+    elif request.method == "GET":
+        return render_template('authenticate.html')
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
